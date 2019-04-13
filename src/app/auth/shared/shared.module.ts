0bbc65279dd41fe2e0d3944e17ecb27ecda26d3d
tral909 <indorm1992@mail.ru>
2019-04-13 23:11:13 +0300

    6 login page. validation

---
+++ src/app/auth/shared/shared.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+@NgModule({
+    imports: [ReactiveFormsModule, FormsModule],
+    exports: [ReactiveFormsModule, FormsModule]
+})
+export class SharedModule {}
