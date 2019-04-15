8be15d399380524fbbdafb79b3124f72ba64851f
tral909 <indorm1992@mail.ru>
2019-04-16 00:04:08 +0300

    15 bill page. get data from srv

--- src/app/system/bill-page/bill-page.component.ts
+++ src/app/system/bill-page/bill-page.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription, combineLatest } from 'rxjs';
+//import { Subscription } from 'rxjs/Subscription';
+
+import { Bill } from '../shared/models/bill.model';
+import { BillService } from '../shared/services/bill.service';
 
 @Component({
   selector: 'regorov-bill-page',
   templateUrl: './bill-page.component.html',
   styleUrls: ['./bill-page.component.scss']
 })
-export class BillPageComponent implements OnInit {
+export class BillPageComponent implements OnInit, OnDestroy {
+    subscription: Subscription;
 
-  constructor() { }
+    constructor(private billService: BillService) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        combineLatest(
+            this.billService.getBill(),
+            this.billService.getCurrency()
+        ).subscribe((data: [Bill, any]) => {
+            console.log(data);
+        });
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 
 }
