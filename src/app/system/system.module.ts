eab767adf38d12a51788bf74e45ec6415efee5e9
tral909 <indorm1992@mail.ru>
2019-04-16 22:36:33 +0300

    18 create pipe for date and time formatting

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -14,6 +14,7 @@ import { DropdownDirective } from './shared/directives/dropdown.directive';
 import { BillCardComponent } from './bill-page/bill-card/bill-card.component';
 import { CurrencyCardComponent } from './bill-page/currency-card/currency-card.component';
 import { BillService } from './shared/services/bill.service';
+import { MomentPipe } from './shared/pipes/moment.pipe';
 
 @NgModule({
     imports: [
@@ -31,7 +32,8 @@ import { BillService } from './shared/services/bill.service';
         HeaderComponent,
         DropdownDirective,
         BillCardComponent,
-        CurrencyCardComponent
+        CurrencyCardComponent,
+        MomentPipe
     ],
     providers: [BillService]
 })
