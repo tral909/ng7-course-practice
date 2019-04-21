07b16eda57dd374dd2a4c66b9a6ff3184921637a
tral909 <indorm1992@mail.ru>
2019-04-21 10:35:30 +0300

    25 history page. template

---
+++ src/app/system/history-page/history-chart/history-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'regorov-history-chart',
+  templateUrl: './history-chart.component.html',
+  styleUrls: ['./history-chart.component.scss']
+})
+export class HistoryChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
