d1b4a341c7d3f6883a17e888d068de9fc316648e
tral909 <indorm1992@mail.ru>
2019-04-21 11:33:41 +0300

    26 history page. chart

--- src/app/system/history-page/history-chart/history-chart.component.ts
+++ src/app/system/history-page/history-chart/history-chart.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'regorov-history-chart',
   templateUrl: './history-chart.component.html',
   styleUrls: ['./history-chart.component.scss']
 })
-export class HistoryChartComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HistoryChartComponent {
+  @Input() data;
 }
--- src/app/system/history-page/history-page.component.ts
+++ src/app/system/history-page/history-page.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { combineLatest, Subscription } from 'rxjs';
+
+import { CategoriesService } from '../shared/services/categories.service';
+import { Category } from '../shared/models/category.model';
+import { EventsService } from '../shared/services/events.service';
+import { RegorovEvent } from '../shared/models/event.model';
 
 @Component({
   selector: 'regorov-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.scss']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  categories: Category[] = [];
+  events: RegorovEvent[] = [];
+  isLoaded = false;
+  chartData = [];
+  s1: Subscription;
+
+  constructor(
+      private categoriesService: CategoriesService,
+      private eventsService: EventsService
+  ) {}
 
   ngOnInit() {
+      this.s1 = combineLatest(
+          this.categoriesService.getCategories(),
+          this.eventsService.getEvents()
+      ).subscribe((data: [Category[], RegorovEvent[]]) => {
+          this.categories = data[0];
+          this.events = data[1];
+          this.calculateChartData();
+          this.isLoaded = true;
+      });
   }
 
+  ngOnDestroy() {
+      if (this.s1) {
+          this.s1.unsubscribe();
+      }
+  }
+
+  calculateChartData(): void {
+      this.chartData = [];
+      this.categories.forEach((cat) => {
+          const catEvents = this.events.filter((e) => e.category === cat.id && e.type === 'outcome');
+          this.chartData.push({
+              name: cat.name,
+              value: catEvents.reduce((total, e) => {
+                  total += e.amount;
+                  return total;
+              }, 0)
+          });
+      });
+  }
 }
